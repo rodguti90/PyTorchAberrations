6f96cdf0763ccef2497317348ac7b8831f07665d
rodrigo <rodrigo.gutierrez-cuevas@espci.fr>
2022-08-26 14:59:45 +0200

    Updated the layers and models to new version

--- PyTorchAberrations/aberration_layers.py
+++ PyTorchAberrations/aberration_layers.py
@@ -3,125 +3,63 @@ from torch.nn import Module
 
 
 
-
-
 ################################################################
-################### AUTOGRAD FUNCTIONS #########################
+###################### AUX FUNCTIONS ###########################
 ################################################################
-     
-class ComplexZernikeFunction(torch.autograd.Function):
-    '''
-    Function that apply a complex Zernike polynomial to the phase of a batch 
-    of compleximages (or a matrix).
-    '''
-    @staticmethod
-    def forward(ctx, input, alpha, j):
-        
-        
-        nx = torch.arange(0,2,2./input.shape[1], dtype = torch.float32)
-        ny = torch.arange(0,2,2./input.shape[2], dtype = torch.float32)
 
-        X0, Y0 = 1.+1./input.shape[1], 1.+1./input.shape[2]
-        X,Y = torch.meshgrid(nx,ny)
-        X = X.to(input.device)-X0
-        Y = Y.to(input.device)-Y0
-        
-        # see https://en.wikipedia.org/wiki/Zernike_polynomials
-        if j == 0:
-            F = torch.ones_like(X)
-        elif j == 1:
-            F = 2*X
-        elif j == 2:
-            F = 2*Y
-        elif j == 3:
-            # Oblique astigmatism
-            F = 2.*(6.**(1/2))*X.mul(Y)
-        elif j == 4:
-            # Defocus
-            F = (3.**(1/2))*(2.*(X**2+Y**2)-1)
-        elif j == 5:
-            # Vertical astigmatism
-            F = (6.**(1/2))*(X**2-Y**2)
+
+def zernike_Z(j, X, Y):
+
+    # see https://en.wikipedia.org/wiki/Zernike_polynomials
+    if j == 0:
+        F = torch.ones_like(X)
+    elif j == 1:
+        F = 2*X
+    elif j == 2:
+        F = 2*Y
+    elif j == 3:
+        # Oblique astigmatism
+        F = 2.*(6.**(1/2))*X.mul(Y)
+    elif j == 4:
+        # Defocus
+        F = (3.**(1/2))*(2.*(X**2+Y**2)-1)
+    elif j == 5:
+        # Vertical astigmatism
+        F = (6.**(1/2))*(X**2-Y**2)
+    else:
+        R = torch.sqrt(X**2+Y**2)
+        THETA = torch.atan2(Y, X)
+        if j == 6:
+            # Vertical trefoil 
+            F = (8.**(1/2))*torch.mul(R**3, torch.sin(3.*THETA))
+        elif j == 7:
+            # Vertical coma
+            F = (8.**(1/2))*torch.mul(3.*R**3-2.*R,torch.sin(3.*THETA))
+        elif j == 8:
+            # Horizontal coma 
+            F = (8.**(1/2))*torch.mul(3.*R**3-2.*R,torch.cos(3.*THETA))
+        elif j == 9:
+            # Oblique trefoil 
+            F = (8.**(1/2))*torch.mul(R**3, torch.cos(3.*THETA))
+        elif j == 10:
+            # Oblique quadrafoil 
+            F = (10.**(1/2))*torch.mul(R**4, torch.sin(4.*THETA))
+        elif j == 11:
+            # Oblique secondary astigmatism 
+            F = (10.**(1/2))*torch.mul(4.*R**4-3.*R**2, torch.sin(2.*THETA))
+        elif j == 12:
+            # Primary spherical
+            F = (5.**(1/2))*(6.*R**4-6.*R**2 + torch.ones_like(R))
+        elif j == 13:
+            # Vertical secondary astigmatism 
+            F = (10.**(1/2))*torch.mul(4.*R**4-3.*R**2, torch.cos(2.*THETA))
+        elif j == 14:
+            # Vertical quadrafoil 
+            F = (10.**(1/2))*torch.mul(R**4, torch.cos(4.*THETA))
         else:
-            R = torch.sqrt(X**2+Y**2)
-            THETA = torch.atan2(Y, X)
-            if j == 6:
-                # Vertical trefoil 
-                F = (8.**(1/2))*torch.mul(R**3, torch.sin(3.*THETA))
-            elif j == 7:
-                # Vertical coma
-                F = (8.**(1/2))*torch.mul(3.*R**3-2.*R,torch.sin(3.*THETA))
-            elif j == 8:
-                # Horizontal coma 
-                F = (8.**(1/2))*torch.mul(3.*R**3-2.*R,torch.cos(3.*THETA))
-            elif j == 9:
-                # Oblique trefoil 
-                F = (8.**(1/2))*torch.mul(R**3, torch.cos(3.*THETA))
-            elif j == 10:
-                # Oblique quadrafoil 
-                F = (10.**(1/2))*torch.mul(R**4, torch.sin(4.*THETA))
-            elif j == 11:
-                # Oblique secondary astigmatism 
-                F = (10.**(1/2))*torch.mul(4.*R**4-3.*R**2, torch.sin(2.*THETA))
-            elif j == 12:
-                # Primary spherical
-                F = (5.**(1/2))*(6.*R**4-6.*R**2 + torch.ones_like(R))
-            elif j == 13:
-                # Vertical secondary astigmatism 
-                F = (10.**(1/2))*torch.mul(4.*R**4-3.*R**2, torch.cos(2.*THETA))
-            elif j == 14:
-                # Vertical quadrafoil 
-                F = (10.**(1/2))*torch.mul(R**4, torch.cos(4.*THETA))
-            else:
-                raise
-        
-        weight = torch.exp(1j*alpha*F)
-        
-        ctx.save_for_backward(input, alpha, F)
-        output = input*weight
-#         output = torch.view_as_real(output)
-        
-        
-#         weight = torch.stack((torch.cos(alpha*F),
-#                              torch.sin(alpha*F)), dim = -1)
-#         ctx.save_for_backward(input, weight, alpha, F)
-        
-#         output = complex_mul(input,weight)
-        
-            
-        
-        
-#         print(input.dtype)
-#         print(alpha.dtype)
-#         print('++'*100)
-        
-        return output
+            raise
     
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, alpha, F = ctx.saved_tensors
-        
-        weight = torch.exp(1j*alpha*F)
-        
-        grad_input = grad_alpha = None
-        if ctx.needs_input_grad[0]:
-#             grad_input = torch.view_as_complex(grad_output)*weight.conj()
-            grad_input = grad_output*weight.conj()
-#             pass
-
-
-        if ctx.needs_input_grad[1]:
-            pass
-            grad_alpha = torch.sum(grad_output*(1j*F*weight*input).conj()).real
-#             print(grad_alpha.dtype)
-#             print('*'*410)
-#             grad_alpha.imag = 0.
-            grad_alpha.unsqueeze_(0)
-            
-        return grad_input, grad_alpha, None
-#         return torch.view_as_real(grad_input), grad_alpha, None
-    
-    
+    return F
 
 #######################################################
 #################### MODULES ##########################
@@ -155,7 +93,18 @@ class ComplexZernike(Module):
 
 
     def forward(self, input):
-        return ComplexZernikeFunction.apply(input, self.alpha, self.j)
+
+        nx = torch.arange(0,2,2./input.shape[1], dtype = torch.float32)
+        ny = torch.arange(0,2,2./input.shape[2], dtype = torch.float32)
+
+        X0, Y0 = 1.+1./input.shape[1], 1.+1./input.shape[2]
+        X,Y = torch.meshgrid(nx,ny)
+        X = X.to(input.device)-X0
+        Y = Y.to(input.device)-Y0
+
+        F = zernike_Z(self.j, X, Y)
+
+        return input * torch.exp(1j*self.alpha*F)
 
 class ComplexScaling(Module):
     '''
--- PyTorchAberrations/aberration_models.py
+++ PyTorchAberrations/aberration_models.py
@@ -2,12 +2,20 @@ import torch
 import numpy as np
 from torch.nn import Module, Sequential, Identity
 from torch.nn import ZeroPad2d
-from .aberration_layers import ComplexDeformation#, CorrectIllumination
+from .aberration_layers import ComplexDeformation
 from .aberration_layers import ComplexZernike, ComplexScaling
-from .aberration_functions import crop_center, complex_fftshift
-from .aberration_functions import complex_ifftshift
-from .aberration_functions import complex_fft, complex_ifft
+# from .aberration_functions import crop_center, complex_fftshift
+# from .aberration_functions import complex_ifftshift
+# from .aberration_functions import complex_fft, complex_ifft
 import torch.fft as fft
+
+def crop_center(input, size):
+    x = input.shape[1]
+    y = input.shape[2]
+    start_x = x//2-(size//2)
+    start_y = y//2-(size//2)
+    return input[:,start_x:start_x+size,start_y:start_y+size,...]
+
 
 class AberrationModes(torch.nn.Module):
     '''
@@ -15,12 +23,12 @@ class AberrationModes(torch.nn.Module):
     Apply an `Aberration` model to the input and output mode basis.
     '''
     def __init__(self, 
-                     inpoints,
-                     onpoints,
-                     padding_coeff = 0.,
-                     list_zernike_ft = list(range(3)),
-                     list_zernike_direct = list(range(3)),
-                     deformation = 'single'):
+                 inpoints,
+                 onpoints,
+                 padding_coeff = 0.,
+                 list_zernike_ft = list(range(3)),
+                 list_zernike_direct = list(range(3)),
+                 deformation = 'single'):
         super(AberrationModes, self).__init__()
 
         
@@ -42,27 +50,25 @@ class AberrationModes(torch.nn.Module):
         else:
             raise ValueError('Zernike coefficients can only have 1 or 2 dims')
         
-
         self.abberation_output = Aberration(onpoints,
                                             list_zernike_ft = list_zernike_ft_out,
                                             list_zernike_direct = list_zernike_direct_out, 
                                             padding_coeff = padding_coeff,
                                             deformation = deformation)
         self.abberation_input = Aberration(inpoints,
-                                            list_zernike_ft = list_zernike_ft_in,
-                                            list_zernike_direct = list_zernike_direct_in, 
-                                            padding_coeff = padding_coeff,
-                                            deformation = deformation)    
+                                           list_zernike_ft = list_zernike_ft_in,
+                                           list_zernike_direct = list_zernike_direct_in, 
+                                           padding_coeff = padding_coeff,
+                                           deformation = deformation)    
         self.inpoints = inpoints
         self.onpoints = onpoints
 
-    def forward(self,input, output):
+    def forward(self, input, output):
         
         output_modes = output
         output_modes = self.abberation_output(output_modes)
         # output_modes = normalize(output_modes.reshape((-1,self.onpoints**2,2)),device = self.device).reshape((-1,self.onpoints,self.onpoints,2))
         
-
         input_modes = input
         input_modes = self.abberation_input(input_modes)
         # input_modes = normalize(input_modes.reshape((-1,self.inpoints**2,2)),device = self.device).reshape((-1,self.inpoints,self.inpoints,2))
@@ -81,7 +87,6 @@ class Aberration(torch.nn.Module):
                  list_zernike_direct,
                  padding_coeff = 0., 
                  deformation = 'single',
-                #  correct_illum = False,
                  features = None):
         # Here we define the type of Model we want to be using, the number of polynoms and if we want to implement a deformation.
         super(Aberration, self).__init__()
@@ -111,10 +116,10 @@ class Aberration(torch.nn.Module):
        
       
     def forward(self,input):
-        assert(input.shape[1] == input.shape[2])
+        assert(input.shape[-1] == input.shape[-2])
         
         # padding
-        input = self.pad(torch.view_as_complex(input))
+        input = self.pad(input)
 
         # scaling
         input = self.deformation(input)
@@ -124,32 +129,20 @@ class Aberration(torch.nn.Module):
         input = fft.ifftshift(input, dim=(-2,-1))
         input = fft.fft2(input)
         input = fft.fftshift(input, dim=(-2,-1))
-        # input = complex_ifftshift(input)
-        # input = complex_fft(input, 2)
-        # input = complex_fftshift(input)
-#         input = torch.view_as_real(complex_fftshift(input))
 
         # Zernike layers in the Fourier plane
         input = self.zernike_ft(input)
 
         # to direct domain
-#         input = torch.view_as_complex(input)
         input = fft.ifftshift(input, dim=(-2,-1))
         input = fft.ifft2(input)
         input = fft.fftshift(input, dim=(-2,-1))
-        # input = complex_ifftshift(input)
-        # input = complex_ifft(input, 2)
-        # input = complex_fftshift(input)
-#         input = torch.view_as_real(input)
          
         # Zernike layers in the direct plane
         input = self.zernike_direct(input)
         
         # Crop at the center (because of coeff) 
         input = crop_center(input,self.nxy)
-
-        # input = self.correct_illum(input)
             
-
-        return torch.view_as_real(input)
+        return input 
       
